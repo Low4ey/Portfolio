3ddaac368427f2fabe3ee7f016208a7f18b552aa
Ajeet Kumar <ajeet8364@gmail.com>
2022-03-16 10:44:25 +0530

    1.009

--- Component/Editing.js
+++ Component/Editing.js
@@ -7,7 +7,7 @@ function Editing() {
         <>
            <div className={styles.web}>
                <Bar name="Photoshop" width="70%" />
-               <Bar name="Blender" width="10%" />
+               <Bar name="Blender" width="20%" />
                <Bar name="Premiere Pro" width="50%" />
                <p>lorem ipsum asohdfosi ghfd gvlbjnd tepr4ah gfdjlnabg aeuroi fgbnadj gaerpiu tadfjlnbg alhtip aergbnfadl gbadeilfht rpeiaugfvbnldafgh aeqr4liuhtgadlkjgb </p>
            </div>
