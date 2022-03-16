e89a86cb03189f62bd363e35a66f0386f47506b6
Tirthan Prajapati <82008300+zero2ditf@users.noreply.github.com>
2022-03-16 10:05:56 +0530

    LMAO
    LMao

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
       <hr />
     </div>
     <div className={styles.container}>
-      <li className={styles.logo}><Image src="/me.png" alt='me' height={700} width={700} /></li>
+      <li className={styles.logo}><Image src="/me.png" alt='me' height={500} width={500} /></li>
       <p className={styles.para}> why idk Ajeet Is my name sjdhsfv eyrvewku fvda urve afds voewtg fkds gfue groewgFHHDSJ GFRTYTDGHF DSF DHSGF REGADFHDJGLlhfjdgbrue bdjh fdja fyradhf lfdalhfgdaelafgldjgfjda railgfjgdflgelrafrtgfldhg raugfaglartaf f glaretirwgfg ggflg ruglsfgre t fhg ehtu qtrjhg ueg</p>
     </div>
 </div>
