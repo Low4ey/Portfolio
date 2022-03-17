313f9b62a9a5e427126bd1a4ec8539782d1f053f
Ajeet Kumar <ajeet8364@gmail.com>
2022-03-17 16:58:40 +0530

    1.012

--- pages/skills.js
+++ pages/skills.js
@@ -4,6 +4,7 @@ import Programming from "../Component/programming";
 import Editing from "../Component/Editing";
 import styles from "../styles/skill.module.css"
 import Btn from "../Component/btn";
+import Tag_cloud from "../Component/Tag_cloud";
 
 function Skill() {
     const [skill, setskill] = useState("Web")
@@ -14,6 +15,9 @@ function Skill() {
                 <a onClick={() => setskill("Programming")} className={styles.skilla}><Btn name="Programming" /></a>
                 <a onClick={() => setskill("Editing")} className={styles.skilla}><Btn name="Editing" /></a>
             </ul>
+            <div className={styles.cld}>
+            <Tag_cloud />
+            </div>
         </div>
         {skill === "Web" && <Web />}
         {skill === "Programming" && <Programming />}
